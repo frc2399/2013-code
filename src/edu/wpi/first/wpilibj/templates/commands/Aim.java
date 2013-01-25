2670ca03e769ffa011e101ab5a8a28447fff6f2d
Lauren Dierker <ldierker14@hb.edu>
2013-01-24 19:46:41 -0500

    more files

---
+++ src/edu/wpi/first/wpilibj/templates/commands/Aim.java
@@ -0,0 +1,54 @@
+
+package edu.wpi.first.wpilibj.templates.commands;
+
+/**
+ *
+ * @author Lauren Dierker
+ */
+public class Aim extends CommandBase {
+    //measured from ground to top of target
+    //These are NOT actually 0!!!
+    final double topHeightFromGround = 0;
+    final double middleHeightFromGround = 0;
+    final double pyramidHeightFromGround = 0;
+    
+    
+    final double topWidth = 0;
+    final double middleWidth = 0;
+    final double pyramidWidth = 0;
+    
+    final double topHeight = 0;
+    final double middleHeight = 0;
+    final double pyramidHeight = 0;
+    
+    final double cameraHeight = 0;
+    
+
+    public Aim() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(CommandBase.pitch);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ src/edu/wpi/first/wpilibj/templates/subsystems/DriveTrain.java
@@ -0,0 +1,31 @@
+
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.templates.commands.JoystickDrive;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.RobotDrive;
+
+/**
+ *
+ * @author Lauren Dierker and Jessie Adkins
+ */
+public class DriveTrain extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    //THESE ARE NOT CORRECT PORT NUMBERS!!!
+    Jaguar leftFront = new Jaguar(1);
+    Jaguar leftRear = new Jaguar(2);
+    Jaguar rightFront = new Jaguar(3);
+    Jaguar rightRear = new Jaguar(4);
+    
+    public RobotDrive drive = new RobotDrive( leftFront, leftRear, rightFront, rightRear);
+    
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand( new JoystickDrive());
+    }
+}
+
