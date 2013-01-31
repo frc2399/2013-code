994a27a818e50519d2756d2e7ab48b24b42efc70
Jessie Adkins <zaheelee@gmail.com>
2013-01-30 19:33:50 -0500

    Started working on PID

--- src/edu/wpi/first/wpilibj/templates/subsystems/Pitch.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Pitch.java
@@ -1,15 +1,19 @@
 
 package edu.wpi.first.wpilibj.templates.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
 /**
  *
  */
-public class Pitch extends Subsystem {
+public class Pitch extends PIDSubsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+    public Pitch(){
+        super(.1, .1, .1);
+    }
+    
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
