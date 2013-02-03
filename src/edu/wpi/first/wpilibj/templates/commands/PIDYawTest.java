0ce6617e6cc5103cc813d2dee0b8819d0ae04b05
Jessie Adkins <zaheelee@gmail.com>
2013-02-03 16:15:06 -0500

    added some files

--- src/edu/wpi/first/wpilibj/templates/commands/PIDYawTest.java
+++ src/edu/wpi/first/wpilibj/templates/commands/PIDYawTest.java
@@ -1,20 +1,30 @@
 
 package edu.wpi.first.wpilibj.templates.commands;
 
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.templates.PIDOutputTranslator;
+
 
 /**
  *
  * @author Jessie
  */
 public class PIDYawTest extends CommandBase {
+    
+    PIDController controller;
+    PIDOutputTranslator translator;
 
     public PIDYawTest() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+        requires(driveTrain);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        translator = new PIDOutputTranslator();
+        
+        controller = new PIDController(0.001, 0, 0, driveTrain.gyro, translator);
     }
 
     // Called repeatedly when this Command is scheduled to run
