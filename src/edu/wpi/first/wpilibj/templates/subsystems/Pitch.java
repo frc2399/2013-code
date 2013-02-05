4083b5fb891d6775f78b4e0e8c2d79f682eb12d4
Jessie Adkins <zaheelee@gmail.com>
2013-02-04 19:53:03 -0500

    added the two classes I made for the pid pitch controls

---
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Pitch.java
@@ -0,0 +1,43 @@
+
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.templates.RobotMap;
+/**
+ *
+ * @author Jessie
+ */
+public class Pitch extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    
+    public Jaguar pitchMot = new Jaguar(RobotMap.pitchMot);
+    public Encoder pitchEncoder = new Encoder(RobotMap.pitchEncoderA, RobotMap.pitchEncoderB);
+    public Pitch(){
+        
+    }
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    /**
+     * sets the speed of the pitch motor.  
+     * @param speed speed of the pitch motor. 0 <= speed <= 1.
+     */
+    public void setSpeed(double speed){
+        pitchMot.set(speed);
+    }
+    
+    /**
+     * 
+     * @return returns the value of the pitch encoder
+     */
+    public double getEncoder(){
+        return pitchEncoder.get();
+    }
+}
+
